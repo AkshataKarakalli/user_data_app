8e595428c602571f82a81ea30b0b4b9f76bb6b3b
AkshataKarakalli <akshatakarakalli@gmail.com>
2021-08-25 18:50:48 +0530

    added search

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -53,6 +53,27 @@ class Api::UsersController < ApplicationController
       end
 
     end
+    
+    def search 
+      if params[:input].blank?
+          # render json: { msg: "Empty field!"}, status: :unprocessable_entity 
+          render json: { msg: "empty field!"}, status: :unprocessable_entity 
+      else  
+          @parameter = params[:input]
+          # render json: @parameter, status: :ok
+         if  @results = User.where(firstName: "#{@parameter}").to_a 
+          render json: @results, status: :ok
+         elsif @results = User.where(lastName: "#{@parameter}").to_a
+          render json: @results, status: :ok
+         elsif @results = User.where(email: "#{@parameter}").to_a
+          render json: @results, status: :ok
+         else
+          render json: { msg: "Not Found!"}, status: :unprocessable_entity  
+         end
+
+          # User.all.where("lower(firstName) LIKE :input",
+      end  
+  end
 
     private
     def userparams
